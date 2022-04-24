dc7ad2af8835336330b47cf2e973beec9073f99d
dinggasukapaus <182410102006@mail.unej.ac.id>
2022-04-25 02:59:34 +0700

    done welcome page and login page

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,72 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          // we will give media query height
-          // double.infinity make it big as my parent allows
-          // while MediaQuery make it big as per the screen
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              // we will give media query height
+              // double.infinity make it big as my parent allows
+              // while MediaQuery make it big as per the screen
 
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Shutter.cam",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/backgrounds/welcom.png"))),
-              ),
-              Column(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Color.fromRGBO(76, 219, 196, 1),
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+              child: Column(
                 children: <Widget>[
-                  MaterialButton(
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => LoginPage()));
-                    },
-                  )
+                  SizedBox(height: 50),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/icons/photo.png',
+                        width: 100,
+                        height: 100,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        "Shutter.cam",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 110),
+                      child: Image.asset(
+                        "assets/backgrounds/welcom.png",
+                        height: 300,
+                      ),
+                    ),
+                  ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                color: Colors.white,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.all(10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      // minimumSize: const Size.fromHeight(
+                      //     50), // fromHeight use double.infinity as width and 40 is the height
+                      ),
+                  child: Text('Login'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/welcome_screen/login.dart
+++ lib/welcome_screen/login.dart
@@ -10,6 +10,137 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Container(
+                // we will give media query height
+                // double.infinity make it big as my parent allows
+                // while MediaQuery make it big as per the screen
+
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Color.fromRGBO(76, 219, 196, 1),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 50),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/icons/photo.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 50),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "selamat datang",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 30),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Silahkan masukkan email dan password akun anda",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                            textAlign: TextAlign.center,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: Column(
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    floatingLabelBehavior:
+                                        FloatingLabelBehavior.never,
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Email',
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    floatingLabelBehavior:
+                                        FloatingLabelBehavior.never,
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Email',
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: const Size.fromHeight(50),
+                                  ),
+                                  child: Text('Login'),
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => LoginPage()));
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(children: <Widget>[
+                                  Expanded(
+                                      child: Divider(
+                                    color: Colors.black,
+                                  )),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text("atau"),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Expanded(
+                                      child: Divider(
+                                    color: Colors.black,
+                                  )),
+                                ]),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Belum memiliki akun ?"),
+                                    Text("Daftar disini"),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
