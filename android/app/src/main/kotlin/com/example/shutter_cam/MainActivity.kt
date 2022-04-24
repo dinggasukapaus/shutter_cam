ac05212d5153d2079ce189dd8e1c6e74328b342b
dinggasukapaus <182410102006@mail.unej.ac.id>
2022-04-24 16:29:21 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/shutter_cam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shutter_cam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          // we will give media query height
+          // double.infinity make it big as my parent allows
+          // while MediaQuery make it big as per the screen
+
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "welcome",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
