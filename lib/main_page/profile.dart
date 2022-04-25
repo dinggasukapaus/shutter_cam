331d892e9c2580c6c055d3875a3e534bf50bdc54
Hendrawan <hendrareansha@gmail.com>
2022-04-25 19:48:39 +0700

    profile

--- lib/main_page/profile.dart
+++ lib/main_page/profile.dart
@@ -11,12 +11,95 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0.0,
-        shadowColor: Colors.transparent,
-        backgroundColor: Color.fromRGBO(76, 219, 196, 1),
-        title: Text("Profile"),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.only(left: 24, right: 24, top: 24),
+          width: MediaQuery.of(context).size.width,
+          color: const Color.fromRGBO(76, 219, 196, 1),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Profile",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // ignore: avoid_unnecessary_containers
+                  Row(
+                    children: [
+                      Container(
+                        child: Image.asset(
+                          'assets/icons/gamer.png',
+                          height: 60.0,
+                          width: 60.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "User123",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "user123@mail.com",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    "Edit",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color(0xFFFFD05B),
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 18, right: 18),
+                width: MediaQuery.of(context).size.width,
+                color: const Color(0xFF3DF6D9),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Image.asset(
+                        'assets/icons/medal.png',
+                        height: 60.0,
+                        width: 60.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
