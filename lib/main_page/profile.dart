9c8e024626534a5862c20ca049790ee6d444273e
Hendrawan <hendrareansha@gmail.com>
2022-04-25 20:23:55 +0700

    profile

--- lib/main_page/profile.dart
+++ lib/main_page/profile.dart
@@ -11,96 +11,335 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xfff1f5f9),
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.only(left: 24, right: 24, top: 24),
-          width: MediaQuery.of(context).size.width,
-          color: const Color.fromRGBO(76, 219, 196, 1),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Profile",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.only(left: 24, right: 24, top: 24, bottom: 24),
+            width: MediaQuery.of(context).size.width,
+            color: const Color.fromRGBO(76, 219, 196, 1),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Profile",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // ignore: avoid_unnecessary_containers
-                  Row(
-                    children: [
-                      Container(
-                        child: Image.asset(
-                          'assets/icons/gamer.png',
-                          height: 60.0,
-                          width: 60.0,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            "User123",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w700),
-                          ),
-                          Text(
-                            "user123@mail.com",
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const Text(
-                    "Edit",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Color(0xFFFFD05B),
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 18, right: 18),
-                width: MediaQuery.of(context).size.width,
-                color: const Color(0xFF3DF6D9),
-                child: Row(
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      child: Image.asset(
-                        'assets/icons/medal.png',
-                        height: 60.0,
-                        width: 60.0,
-                        fit: BoxFit.cover,
+                    // ignore: avoid_unnecessary_containers
+                    Row(
+                      children: [
+                        Container(
+                          child: Image.asset(
+                            'assets/icons/gamer.png',
+                            height: 60.0,
+                            width: 60.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "User123",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              "user123@mail.com",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const Text(
+                      "Edit",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Color(0xFFFFD05B),
+                        fontWeight: FontWeight.w800,
                       ),
                     ),
                   ],
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 18,
+                    right: 18,
+                    top: 11,
+                    bottom: 11,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF3DF6D9),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(2, 3),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              'assets/icons/medal.png',
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Verified Your Member",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Image.asset(
+                          'assets/icons/right-chevron.png',
+                          height: 30.0,
+                          width: 30.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
+          Container(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 18,
+                    right: 18,
+                    top: 11,
+                    bottom: 11,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              'assets/icons/list.png',
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Terms and Condition",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Image.asset(
+                          'assets/icons/right-chevron.png',
+                          height: 30.0,
+                          width: 30.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 18,
+                    right: 18,
+                    top: 11,
+                    bottom: 11,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              'assets/icons/lock.png',
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Privacy Policy",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Image.asset(
+                          'assets/icons/right-chevron.png',
+                          height: 30.0,
+                          width: 30.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 18,
+                    right: 18,
+                    top: 11,
+                    bottom: 11,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              'assets/icons/setting.png',
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Setting",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Image.asset(
+                          'assets/icons/right-chevron.png',
+                          height: 30.0,
+                          width: 30.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 18,
+                    right: 18,
+                    top: 11,
+                    bottom: 11,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              'assets/icons/logout.png',
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Log Out",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Image.asset(
+                          'assets/icons/right-chevron.png',
+                          height: 30.0,
+                          width: 30.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
