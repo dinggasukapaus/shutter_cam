69816b78e28ef91a0cad3ceab73bc491bc31bf4c
Hendrawan <hendrareansha@gmail.com>
2022-04-25 21:02:36 +0700

    fix profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:shutter_cam/main_page/bottomNavigate.dart';
-import 'package:shutter_cam/main_page/camera.dart';
-
-import 'package:shutter_cam/welcome_screen/login.dart';
-import 'package:shutter_cam/welcome_screen/register.dart';
-import 'package:shutter_cam/welcome_screen/splash.dart';
-import 'package:shutter_cam/main_page/dashboard.dart';
-import 'package:shutter_cam/main_page/camera.dart';
-import 'package:shutter_cam/main_page/bottomNavigate.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: BottomNavigate(),
+  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Color(0xFF4CDBC4),
   ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavigate(),
+    );
+  }
 }
