f0946e23c2a33b0759d033539d0f9db418402ad9
dinggasukapaus <182410102006@mail.unej.ac.id>
2022-06-05 18:26:47 +0700

    done atur penyewaan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shutter_cam/welcome_screen/splash.dart';
+// import 'package:shutter_cam/main_page/arrangeRental.dart';
 import 'package:flutter/services.dart';
 
 void main() {
